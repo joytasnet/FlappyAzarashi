c3751f2cc3eadecf7ed3691691767830f608cd90
mjpurin <mjpurin@gmail.com>
2024-03-25 14:08:55 +0900

    p375

--- Assets/Scripts/AzarashiController.cs
+++ Assets/Scripts/AzarashiController.cs
@@ -8,11 +8,16 @@ public class AzarashiController : MonoBehaviour
     Rigidbody2D rb2d;
     Animator animator;
     float angle;
+    bool isDead;
 
     public float maxHeight;
     public float flapVelocity;
     public float relativeVelocityX;
     public GameObject sprite;
+
+    public bool IsDead(){
+        return isDead;
+    }
 
     void Awake()
     {
@@ -26,14 +31,24 @@ public class AzarashiController : MonoBehaviour
             Flap();
         }
         ApplyAngle();
-        animator.SetBool("flap",angle >0f);
+        animator.SetBool("flap",angle >0f && !isDead);
     }
     public void Flap(){
+        if(isDead) return;
         rb2d.velocity=new Vector2(0f,flapVelocity);
     }
     void ApplyAngle(){
-        float targetAngle=Mathf.Atan2(rb2d.velocity.y,relativeVelocityX)*Mathf.Rad2Deg;
+        float targetAngle;
+        if(isDead){
+            targetAngle=180f;
+        }else{
+            targetAngle=Mathf.Atan2(rb2d.velocity.y,relativeVelocityX)*Mathf.Rad2Deg;
+        }
         angle = Mathf.Lerp(angle,targetAngle,Time.deltaTime * 10f);
         sprite.transform.localRotation = Quaternion.Euler(0f,0f,angle);
+    }
+    void OnCollisionEnter2D(Collision2D collision){
+        if(isDead) return;
+        isDead=true;
     }
 }
